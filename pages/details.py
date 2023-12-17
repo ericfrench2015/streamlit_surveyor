a445f2abab371c17f7b18a6070b495c93ef8db77
Eric French <ericfrench2015@gmail.com>
2023-12-17 15:10:08 -0600

    add mapping and default files uploaded

--- pages/details.py
+++ pages/details.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import pandas as pd
+import geopandas as gpd
 from collections import Counter
 import matplotlib.pyplot as plt
+
+@st.cache_resource
+def load_map(shx_file):
+    map_df = gpd.read_file(shx_file)
+    return map_df
+
 
 @st.cache_resource
 def load_data(uploaded_file):
@@ -26,28 +33,55 @@ This is a proof of concept to see what is possible by leveraging secondary data 
 desc_text = """Type in a date in YYYY-MM-DD format. This will serve as the simulated present."
          In other words, if you type in 2023-08-01, this app will behave as if that is today's
          date and all results will appear as such."""
-date_of_reference = st.text_input(f"Date of Reference: {desc_text}", '2023-12-10')
 
-uploaded_file = st.file_uploader("Upload a situation_report preprocessed file")
+shx_file = "data//tur_polbnda_adm1.shx"
+map_df = load_map(shx_file)
+#map_df.plot(figsize=(20, 10))
+#st.pyplot(map_df)
 
-if uploaded_file:
+plt.figure(figsize=(10, 5))
+map_df.plot(ax=plt.gca())
+plt.axis('off')
+
+col1, col2 = st.columns(2)
+with col1:
+    st.pyplot(plt)
+
+#x.set_axis_off()
+
+
+
+#st.pyplot(map_df.plot(cmap='seismic_r', figsize=(20, 10)))
+with col2:
+    date_of_reference = st.text_input(f"Date of Reference: {desc_text}", '2023-12-10')
+
+#uploaded_file = st.file_uploader("Upload a situation_report preprocessed file")
+
+#if uploaded_file:
     #st.write(f"Filename: {uploaded_file}")
     #df_full = pd.read_excel(uploaded_file)
-    df_full = load_data(uploaded_file)
+#    df_full = load_data(uploaded_file)
+#else:
+df_full = load_data("data//situation_reports.xlsx")
 
 
-    df = df_full[df_full['reported_date'] <= date_of_reference]
 
-    st.write("Below is a random sampling of 2 df rows so you can see what columns are in the data.")
-    st.dataframe(df_full.sample(2),use_container_width=True)
 
-    st.subheader("Indicator Selector: Filter statements to your preferred topic area")
-    st.write("Each indicator has a set of keywords (TODO: Expose these for transparency)"
-             "select one and you'll see statements related to that area.")
-    indicators = get_indicator_columns(df)
-    ind_opt = st.selectbox('indicator_selector', indicators)
-    st.write("Here are the top 20 most recent statements ", ind_opt)
 
-    df_show = df[df[ind_opt] == 1]
-    st.dataframe(df_show[['reported_date','identified_adm_chain','spacy_sent_no_paren','authoring_org','source_url']].sort_values(by='reported_date', ascending=False))
+
+
+df = df_full[df_full['reported_date'] <= date_of_reference]
+
+st.write("Below is a random sampling of 2 df rows so you can see what columns are in the data.")
+st.dataframe(df_full.sample(2),use_container_width=True)
+
+st.subheader("Indicator Selector: Filter statements to your preferred topic area")
+st.write("Each indicator has a set of keywords (TODO: Expose these for transparency)"
+         "select one and you'll see statements related to that area.")
+indicators = get_indicator_columns(df)
+ind_opt = st.selectbox('indicator_selector', indicators)
+st.write("Here are the top 20 most recent statements ", ind_opt)
+
+df_show = df[df[ind_opt] == 1]
+st.dataframe(df_show[['reported_date','identified_adm_chain','spacy_sent_no_paren','authoring_org','source_url']].sort_values(by='reported_date', ascending=False))
 
--- topic_surveyor.py
+++ topic_surveyor.py
@@ -22,88 +22,72 @@ with col2:
                  date and all results will appear as such."""
     date_of_reference = st.text_input(f"Date of Reference: {desc_text}", '2023-12-10')
 
+st.write("testing loading a file from app")
+df_full = pd.read_excel("data//ongoing.xlsx")
+
+#st.dataframe(df_test.head(4))
 
 if uploaded_file:
     #st.write(f"Filename: {uploaded_file}")
     df_full = pd.read_excel(uploaded_file)
 
-    #df['identified_country'] = df['identified_country'].astype(str)
-    #df['reference_date_iso'] = pd.to_datetime(df['reference_date_iso'])
 
 
 
-    #with col1:
-    #    countries = Counter(df['identified_country'].tolist())
-    #    st.write("Top 10 countries by reference count in uploaded file")
-    #    df_c = pd.DataFrame(countries.most_common(10), columns=['country','reference_count'])
-    #    st.write(df_c)
+df = df_full[df_full['reference_date_iso'] <= date_of_reference]
+col1a, col2a = st.columns(2)
+#df['max_count'] = df.groupby('identified_country')['num_killed_int'].expanding().max().reset_index(level=0, drop=True)
 
-    #with col2:
+df = df[(df['identified_country'] == 'Türkiye') & (df['glide_id'] == 'EQ-2023-000015-TUR')]
+df = df.sort_values(by='reference_date_iso')
 
-    #desc_text = """Type in a date in YYYY-MM-DD format. This will serve as the simulated present."
-    #         In other words, if you type in 2023-08-01, this app will behave as if that is today's
-    #         date and all results will appear as such."""
-    #date_of_reference = st.text_input(f"Date of Reference: {desc_text}", '2023-12-10')
+df['max_count'] = df.groupby('identified_country')['num_killed_int'].expanding().max().reset_index(level=0,
+                                                                                                   drop=True)
+df['max_count'] = df['max_count'].fillna(0)
+df = df.dropna(subset=['reference_date_iso']).copy()
 
-    #countries = Counter(df['identified_country'][df['reference_date_iso'] < date_of_reference].tolist())
-    #st.write(f"Top 10 countries by reference count in uploaded file given the reference date of {date_of_reference}")
-    #df_c = pd.DataFrame(countries.most_common(10), columns=['country','reference_count'])
-    #st.write(df_c)
+plt.figure(figsize=(10, 6))
+df = df.sort_values(by=['identified_country', 'reference_date_iso'])
+for country, data in df.groupby('identified_country'):
+    # print(data['reference_date_iso'], data['reference_date_iso'])
+    plt.plot(data['reference_date_iso'], data['max_count'], label=f"{country}: killed", color='red')
 
-    df = df_full[df_full['reference_date_iso'] <= date_of_reference]
-    col1a, col2a = st.columns(2)
-    #df['max_count'] = df.groupby('identified_country')['num_killed_int'].expanding().max().reset_index(level=0, drop=True)
+plt.xlabel('Date')
+plt.ylabel('Max Count')
+plt.title('Max Reported Number of killed in Türkiye over Time (Max from Prior or Current Period)')
+plt.legend()
+plt.grid(False)
+plt.xticks(rotation=90)
 
-    df = df[(df['identified_country'] == 'Türkiye') & (df['glide_id'] == 'EQ-2023-000015-TUR')]
-    df = df.sort_values(by='reference_date_iso')
-
-    df['max_count'] = df.groupby('identified_country')['num_killed_int'].expanding().max().reset_index(level=0,
-                                                                                                       drop=True)
-    df['max_count'] = df['max_count'].fillna(0)
-    df = df.dropna(subset=['reference_date_iso']).copy()
-
-    plt.figure(figsize=(10, 6))
-    df = df.sort_values(by=['identified_country', 'reference_date_iso'])
-    for country, data in df.groupby('identified_country'):
-        # print(data['reference_date_iso'], data['reference_date_iso'])
-        plt.plot(data['reference_date_iso'], data['max_count'], label=f"{country}: killed", color='red')
-
-    plt.xlabel('Date')
-    plt.ylabel('Max Count')
-    plt.title('Max Reported Number of killed in Türkiye over Time (Max from Prior or Current Period)')
-    plt.legend()
-    plt.grid(False)
-    plt.xticks(rotation=90)
-
-    with col1a:
-        st.pyplot(plt)
+with col1a:
+    st.pyplot(plt)
 
 
-    df = df[(df['identified_country'] == 'Türkiye')]
-    df = df.sort_values(by='reference_date_iso')
+df = df[(df['identified_country'] == 'Türkiye')]
+df = df.sort_values(by='reference_date_iso')
 
-    df['max_count'] = df.groupby('identified_country')['num_injured_int'].expanding().max().reset_index(level=0,
-                                                                                                       drop=True)
-    df['max_count'] = df['max_count'].fillna(0)
-    df = df.dropna(subset=['reference_date_iso']).copy()
+df['max_count'] = df.groupby('identified_country')['num_injured_int'].expanding().max().reset_index(level=0,
+                                                                                                   drop=True)
+df['max_count'] = df['max_count'].fillna(0)
+df = df.dropna(subset=['reference_date_iso']).copy()
 
-    plt.figure(figsize=(10, 6))
-    df = df.sort_values(by=['identified_country', 'reference_date_iso'])
-    for country, data in df.groupby('identified_country'):
-        # print(data['reference_date_iso'], data['reference_date_iso'])
-        plt.plot(data['reference_date_iso'], data['max_count'], label=f"{country}: injured")
+plt.figure(figsize=(10, 6))
+df = df.sort_values(by=['identified_country', 'reference_date_iso'])
+for country, data in df.groupby('identified_country'):
+    # print(data['reference_date_iso'], data['reference_date_iso'])
+    plt.plot(data['reference_date_iso'], data['max_count'], label=f"{country}: injured")
 
-    plt.xlabel('Date')
-    plt.ylabel('Max Count')
-    plt.title('Max Reported Number Injured in Türkiye Time (Max from Prior or Current Period)')
-    plt.legend()
-    plt.grid(False)
-    plt.xticks(rotation=90)
-    with col2a:
-        st.pyplot(plt)
+plt.xlabel('Date')
+plt.ylabel('Max Count')
+plt.title('Max Reported Number Injured in Türkiye Time (Max from Prior or Current Period)')
+plt.legend()
+plt.grid(False)
+plt.xticks(rotation=90)
+with col2a:
+    st.pyplot(plt)
 
-    st.subheader("Sorted statements related to number killed")
-    st.write("pandas df wrapping issue. If you want to see the full content of the source text, right-click,"
-             "then left click it.... just go with it")
-    df_show = df[['reference_date_iso','source_original_text','reference_auth_org']][df['num_killed_int'] > 0]
-    st.dataframe(df_show.sort_values(by='reference_date_iso', ascending=False),use_container_width=True)
+st.subheader("Sorted statements related to number killed")
+st.write("pandas df wrapping issue. If you want to see the full content of the source text, right-click,"
+         "then left click it.... just go with it")
+df_show = df[['reference_date_iso','source_original_text','reference_auth_org']][df['num_killed_int'] > 0]
+st.dataframe(df_show.sort_values(by='reference_date_iso', ascending=False),use_container_width=True)
